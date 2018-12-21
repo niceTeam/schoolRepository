1df8fac6ed762294e879939d9f37fea6a8c7b069
zml999 <45159687+zml999@users.noreply.github.com>
2018-12-21 11:24:04 +0800

    云题库架构

--- classcloud/src/main/java/com/accp/spring/ClasscloudApplication.java
+++ classcloud/src/main/java/com/accp/spring/ClasscloudApplication.java
@@ -1,9 +1,11 @@
 package com.accp.spring;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages="com.accp.spring")
 public class ClasscloudApplication {
 
 	public static void main(String[] args) {
