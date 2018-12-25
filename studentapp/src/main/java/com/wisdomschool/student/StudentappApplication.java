0c40653a7dc0101316edc14c04f9e54b418d4664
1603015539 <45160260+1603015539@users.noreply.github.com>
2018-12-25 16:56:02 +0800

    AAAAA

--- studentapp/src/main/java/com/wisdomschool/student/StudentappApplication.java
+++ studentapp/src/main/java/com/wisdomschool/student/StudentappApplication.java
@@ -3,9 +3,11 @@ package com.wisdomschool.student;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan(basePackages= {"com.wisdomschool.student.mapper"})
+@ComponentScan(basePackages = {"com.wisdomschool.student.pojo"})
 public class StudentappApplication {
 
 	public static void main(String[] args) {
